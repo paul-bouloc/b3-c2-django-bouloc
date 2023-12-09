43a4075d21ed746f99d1d663c457fa05a2ce5405
moicestpaul <bouloc.p@gmail.com>
2023-12-09 23:56:01 +0100

    Fix model files

--- users/admin.py
+++ users/admin.py
@@ -1,18 +1,7 @@
-from django.db import models
+from django.contrib import admin
+from users.models import Passwords
 
 # Register your models here.
 
-class Passwords(models.Model):
-  user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-  name = models.CharField(max_length=100)
-  website = models.CharField(max_length=100)
-  login = models.CharField(max_length=100)
-  password = models.CharField(max_length=100)
-
-  def __str__(self):
-      return self.name
-    
-  def getNameWebsiteLogin(self):
-    return {'name': self.name, 'website': self.website, 'login': self.login}
-  
+admin.site.register(Passwords) 
   
--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Passwords(models.Model):
+  user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+  name = models.CharField(max_length=100)
+  website = models.CharField(max_length=100)
+  login = models.CharField(max_length=100)
+  password = models.CharField(max_length=100)
+
+  def __str__(self):
+      return self.name
+    
+  def getNameWebsiteLogin(self):
+    return {'name': self.name, 'website': self.website, 'login': self.login}
