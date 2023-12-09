cf1663bc33035cc6072c928c836f1b5cda9180d3
moicestpaul <bouloc.p@gmail.com>
2023-12-09 23:37:07 +0100

    Created passwords model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+from django.db import models
 
 # Register your models here.
+
+class Passwords(models.Model):
+  user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+  name = models.CharField(max_length=100)
+  website = models.CharField(max_length=100)
+  login = models.CharField(max_length=100)
+  password = models.CharField(max_length=100)
+
+  def __str__(self):
+      return self.website
