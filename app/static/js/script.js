cb656a3962ea4f4fa9a92103ed33b8376183a899
moicestpaul <bouloc.p@gmail.com>
2023-12-10 23:19:36 +0100

    Added password generation on edit form

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -8,3 +8,18 @@ function hideFormPassword() {
         document.getElementById("hidePassword").innerHTML = "Show password";
     }
 }
+
+function generateStrongPassword(){
+    var length = 15,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}:<>?|[];',./",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
+    document.getElementById("password").value = retVal;
+
+    var x = document.getElementById("password");
+    if(x.type === "password"){
+        hideFormPassword()
+    }
+}
